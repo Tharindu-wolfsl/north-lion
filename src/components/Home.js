72dbd8804312d488f4cfbab9fcff5285b428839a
wolfsl <tharindupriyashan18@gmail.com>
2022-09-17 15:13:07 +0530

    Slide added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import '../App.css';
+import Carousel from "framer-motion-carousel";
+import slide1 from "./layouts/carousel/1.svg"
+import slide2 from "./layouts/carousel/2.svg"
+import slide3 from "./layouts/carousel/3.svg"
+import slide4 from "./layouts/carousel/4.svg"
+
 
 const Home = () => {
+
+  const images=[slide1,slide2,slide3,slide4]
   return (
     <div className='home container'>
       <div className='head-title'>
        
       </div>
+    
+      <div style={{ width: 700, height: 400, margin: "0 auto" }}>
+  <Carousel>
+    {images.map((item, i) => (
+      <img
+        draggable="false"
+        src={`${item}`}
+        key={i}
+        width="100%"
+        alt=""
+      />
+    ))}
+  </Carousel>
+</div>
+      
+
     </div>
   )
 }
